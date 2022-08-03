c65ae7a1000bff5fe0c52d6b93b0fae1b10bee2a
DrCloudFactory454 <diru@cloudfactory.dk>
2022-08-03 21:36:46 +0200

    deploy.sh updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
-import vitePluginString from "vite-plugin-string";
+
 
 export default defineConfig ({
-  plugins: [vitePluginString.default()],
-  base:'/vite-react-example-454/'
+ base:'/vite-react-example-454/'
 });
